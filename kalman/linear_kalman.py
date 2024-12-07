4a203f08e87418d817ebd6360abd9d0ac6360127
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2024-12-07 17:16:26 +0800

    kalman one api

--- kalman/linear_kalman.py
+++ kalman/linear_kalman.py
@@ -33,7 +33,8 @@ class LinearKalman:
             None, None, H=np.hstack([np.eye(dim), np.zeros((dim, dim))])
         )
 
-    def init(self, z, t):
+    # order: time, data, variance
+    def init(self, t, z):
         self.t = t
         self.kalman.x = np.hstack([z, np.zeros(self.dim)])
         self.kalman.P = np.zeros((self.dim * 2, self.dim * 2))
@@ -51,21 +52,61 @@ class LinearKalman:
         self.kalman.update(z, R)
 
 
+class LinearKalmanOneApi:
+    def __init__(self, dim, dead_time, R, Q):
+        self.linear_kalman = LinearKalman(dim)
+        self.dead_time = dead_time
+        self.Q = Q
+        self.R = R
+
+    def smoothed(self, t, z):
+        if self.linear_kalman.t is None or t - self.linear_kalman.t > self.dead_time:
+            self.linear_kalman.init(t, z)
+        self.linear_kalman.predict(t, self.Q)
+        self.linear_kalman.update(z, self.R)
+        return self.linear_kalman.kalman.x[: self.linear_kalman.dim]
+
+
+def test_linear_kalman_one_api():
+    dim = 3
+    dead_time = 0.1
+    R = np.eye(dim) * 5.0
+    Q = np.eye(dim * 2) * 0.2
+    for i in range(dim):
+        Q[i + dim, i + dim] = 100.0
+    lkoa = LinearKalmanOneApi(dim, dead_time, R, Q)
+    times = np.arange(0, 2, 0.03)
+    measurements = np.tile(
+        np.maximum(np.abs(times - 1) * 2, 1)[:, np.newaxis], (1, 3)
+    ) + np.random.normal(0, 0.02, (len(times), dim))
+    predictions = []
+
+    for i, t in enumerate(times):
+        predictions.append(lkoa.smoothed(t, measurements[i]))
+
+    predictions = np.array(predictions)
+    plt.figure(figsize=(12, 8))
+    for i in range(dim):
+        plt.subplot(dim, 1, i + 1)
+        plt.plot(times, measurements[:, i], label="Measurements")
+        plt.plot(times, predictions[:, i], label="Kalman Filter Prediction")
+        plt.xlabel("Time")
+        plt.ylabel(f"Dimension {i + 1}")
+        plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
 def test_linear_kalman():
-    # 初始化
     dim = 3
     lk = LinearKalman(dim)
-
     z = np.array([1.0, 2.0, 3.0])
     t = 0.0
-    lk.init(z, t)
-
-    # Q: 前三维为 0.1，后三维为 10
+    lk.init(t, z)
     Q = np.eye(dim * 2) * 0.2
     for i in range(dim):
         Q[i + dim, i + dim] = 100.0
     R = np.eye(dim) * 5.0  # 观测噪声协方差矩阵
-
     times = np.arange(0, 2, 0.03)
     measurements = np.tile(
         np.maximum(np.abs(times - 1) * 2, 1)[:, np.newaxis], (1, 3)
@@ -96,4 +137,5 @@ def test_linear_kalman():
 
 # 运行测试
 if __name__ == "__main__":
-    test_linear_kalman()
+    # test_linear_kalman()
+    test_linear_kalman_one_api()
