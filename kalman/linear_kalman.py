862965907d2b6a8be23cb9590e7f2cf555e4ef44
Julyfun jetson nano 241206 <you@example.com>
2024-12-17 16:52:14 +0800

    type annotations

--- kalman/linear_kalman.py
+++ kalman/linear_kalman.py
@@ -3,16 +3,16 @@ from matplotlib import pyplot as plt
 
 
 class Kalman:
-    def __init__(self, x, P, H):
+    def __init__(self, x: np.ndarray, P: np.ndarray, H: np.ndarray):
         self.x = x  # [state * 1]
         self.P = P  # [state * state]
         self.H = H  # [obs * state]
 
-    def predict(self, F, Q):
+    def predict(self, F: np.ndarray, Q: np.ndarray):
         self.x = F @ self.x
         self.P = F @ self.P @ F.T + Q
 
-    def update(self, z, R):
+    def update(self, z: np.ndarray, R: np.ndarray):
         y = z - self.H @ self.x
         S = self.H @ self.P @ self.H.T + R
         K = self.P @ self.H.T @ np.linalg.inv(S)
